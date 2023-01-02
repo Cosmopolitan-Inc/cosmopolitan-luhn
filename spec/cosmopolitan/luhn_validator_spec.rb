aed5f0e7f880b50a1281741e4fedd25055cb0d1a
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-01-02 19:50:31 +0100

    [12] update every double quotes to single quotes

--- spec/cosmopolitan/luhn_validator_spec.rb
+++ spec/cosmopolitan/luhn_validator_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Cosmopolitan::LuhnValidator do
-  it "version is equal to 0.1.0" do
-    expect(Cosmopolitan::LuhnValidator::VERSION).to eq("0.1.0")
+  it 'version is equal to 0.1.0' do
+    expect(Cosmopolitan::LuhnValidator::VERSION).to eq('0.1.0')
   end
 end
