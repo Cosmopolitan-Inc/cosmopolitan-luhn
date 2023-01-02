01e74e7c3f58c0442453887e46374239659eabd0
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-01-02 18:57:02 +0100

    [1] initialize Cosmopolitan::LuhnValidator gem - include Ruby 3.1.2, RSpec, GitHub Actions

---
+++ lib/cosmopolitan/luhn_validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "luhn_validator/version"
+
+module Cosmopolitan
+  module LuhnValidator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cosmopolitan/luhn_validator/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Cosmopolitan
+  module LuhnValidator
+    VERSION = "0.1.0"
+  end
+end
