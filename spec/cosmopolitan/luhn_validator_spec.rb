90c3a8f9c57c648db74da0834c3656dec6f05bd0
Mateusz Tomczak <99652987+tomczak-mateusz@users.noreply.github.com>
2023-01-02 19:22:45 +0100

    [8] update 'has a version number' spec to the one equal to the current value (#7)

--- spec/cosmopolitan/luhn_validator_spec.rb
+++ spec/cosmopolitan/luhn_validator_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Cosmopolitan::LuhnValidator do
-  it "has a version number" do
-    expect(Cosmopolitan::LuhnValidator::VERSION).not_to be nil
+  it "version is equal to 0.1.0" do
+    expect(Cosmopolitan::LuhnValidator::VERSION).to eq("0.1.0")
   end
 end
